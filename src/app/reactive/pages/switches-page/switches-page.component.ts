56d8fd35402bf82b6d356cfa79a6d864d091e012
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-27 07:59:58 -0300

    feat(formulario switches): agrega logica al formulario

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class SwitchesPageComponent {
 
+  public myForm: FormGroup = this.fb.group({
+    gender: ['', Validators.required ],
+    wantNotifications: [false, Validators.required ],
+    termsAndConditions: [false, Validators.requiredTrue ] //revisar en el html el error que da
+  })
+
+  isValidField( field: string ): boolean | null {
+    return this.myForm.controls[field].errors && this.myForm.controls[field].touched
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for( const key of Object.keys(errors) ){
+      switch( key ){
+        case 'required':
+          return 'Este campo es requerido'
+      }
+
+      return null;
+    }
+
+    return ''
+  }
+
+  constructor(private fb: FormBuilder){}
+
+  onSave() {
+    if ( this.myForm.invalid ){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value)
+  }
 }
