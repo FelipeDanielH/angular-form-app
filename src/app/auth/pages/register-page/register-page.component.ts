f239f6c273e41f431a54817b3da517a889e0ba30
Felipe <felipe.daniel.henriquez@gmail.com>
2023-10-27 08:00:27 -0300

    feat(formulario registro): agrega logica al formulario y validaciones basicas

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from 'src/app/shared/validators/validators';
 
 @Component({
-  templateUrl: './register-page.component.html',
-  styles: [
-  ]
+  templateUrl: './register-page.component.html'
 })
 export class RegisterPageComponent {
+
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required] ],
+    email:['', [Validators.required] ],
+    username: ['', [Validators.required] ],
+    password: ['', [Validators.required, Validators.minLength(6)] ],
+    password2: ['', [Validators.required, Validators.minLength(6)] ]
+  });
+
+  constructor (private fb: FormBuilder){}
+
+  isValidField( field: string ){
+    // TODO obtener validacion desde un servicio
+  }
+
+  onSubmit() {
+    this.myForm.markAllAsTouched();
+  }
 
 }
